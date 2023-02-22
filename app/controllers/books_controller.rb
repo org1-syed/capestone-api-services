392dc99247e08747000b43d90afca305f4ad10db
syed-burhan <syedhsjb@gmail.com>
2023-02-22 09:24:24 +0530

    added GET by id and DELETE endpoint

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
+
+  before_action :set_book, only: [:show, :destroy]
+
   def index
     render json: Book.all
+  end
+
+  def show
+    render json: @book
   end
 
   def create
@@ -13,9 +20,20 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book.destroy
+
+    head :no_content
+  end
+
   private
 
   def book_params
     params.permit(:title, :author)    # permit- only these params to be posted not others
+  end
+
+  def set_book
+    @book = Book.find_by(id: params[:id])
+    render plain: 'Book not found', status: :not_found if @book.blank?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 
-  resources :books, only: [:index, :create]
+  resources :books
 end
